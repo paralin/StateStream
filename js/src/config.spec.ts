c7bad06d5eb08092db43d66e0d9d15bd9ba2f164
Christian Stewart <christian@paral.in>
2016-11-06 22:53:51 -0500

    feat(js): implement cursor

--- js/src/config.spec.ts
+++ js/src/config.spec.ts
@@ -1,4 +1,8 @@
-import { Config, RateConfig } from './config';
+import {
+  Config,
+  RateConfig,
+  DefaultStreamConfig,
+} from './config';
 
 describe('config', () => {
   it('should create a Config properly', () => {
@@ -14,5 +18,9 @@ describe('config', () => {
       keyframe_frequency: 10,
     });
     expect(conf.keyframe_frequency + '').toBe('10');
+  });
+  it('should create a valid default config', () => {
+    let conf: any = DefaultStreamConfig();
+    expect(conf.record_rate).not.toBe(null);
   });
 });
--- js/src/index.ts
+++ js/src/index.ts
@@ -1,1 +1,6 @@
 export * from './config';
+export * from './cursor';
+export * from './backend';
+export * from './entry';
+export * from './errors';
+export * from './proto';
