2a44403588640852ee2809e26b702eb59d622e31
Christian Stewart <christian@paral.in>
2016-11-21 11:40:07 -0500

    chore(package): jenkins improvements

--- scripts/jenkins_setup_deps.bash
+++ scripts/jenkins_setup_deps.bash
@@ -2,14 +2,14 @@
 set +x
 set -e
 
-source ./scripts/jenkins_env.bash
+source ./jenkins_scripts/jenkins_env.bash
 
 mkdir -p ./goworkspace/bin
 mkdir -p ./goworkspace/src/github.com/fuserobotics
 ln -fs $(pwd) ./goworkspace/src/github.com/fuserobotics/statestream
 
 pushd ./goworkspace/src/github.com/fuserobotics/statestream
-go get -v ./
+go get -d -v ./
 popd
 
 set -x
