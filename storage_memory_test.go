9e34a845b272950831c008bb60728ba52aca13a9
Christian Stewart <christian@paral.in>
2017-01-05 20:16:26 -0800

    chore: add test for memory backend

---
+++ storage_memory_test.go
@@ -0,0 +1,39 @@
+package stream
+
+import (
+	"testing"
+	"time"
+)
+
+var TestBaseTime = time.Now()
+
+func MockEntries() []*StreamEntry {
+	res := []*StreamEntry{}
+	for i := 0; i < 10; i++ {
+		typ := StreamEntryMutation
+		if i == 0 || i == 5 {
+			typ = StreamEntrySnapshot
+		}
+		res = append(res, &StreamEntry{
+			Data: StateData{
+				"test": i + 1,
+			},
+			Timestamp: TestBaseTime.Add(time.Duration(-10+i) * time.Second),
+			Type:      typ,
+		})
+	}
+	return res
+}
+
+func TestMemoryStorage(t *testing.T) {
+	mb := &MemoryBackend{Entries: MockEntries()}
+	se, _ := mb.GetEntryAfter(
+		TestBaseTime.Add(time.Duration(-9)*time.Second),
+		StreamEntrySnapshot)
+	if se == nil {
+		t.Fail()
+	}
+	if se.Data["test"].(int) != 6 {
+		t.Fail()
+	}
+}
