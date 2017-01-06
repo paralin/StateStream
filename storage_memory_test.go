464b9c1ff37afa81bcf187664b580c6d2103940b
Christian Stewart <christian@paral.in>
2017-01-05 20:17:34 -0800

    chore: add additional test for getsnapshotbefore

--- storage_memory_test.go
+++ storage_memory_test.go
@@ -25,7 +25,7 @@ func MockEntries() []*StreamEntry {
 	return res
 }
 
-func TestMemoryStorage(t *testing.T) {
+func TestEntryAfter(t *testing.T) {
 	mb := &MemoryBackend{Entries: MockEntries()}
 	se, _ := mb.GetEntryAfter(
 		TestBaseTime.Add(time.Duration(-9)*time.Second),
@@ -34,6 +34,17 @@ func TestMemoryStorage(t *testing.T) {
 		t.Fail()
 	}
 	if se.Data["test"].(int) != 6 {
+		t.Fail()
+	}
+}
+
+func TestSnapshotBefore(t *testing.T) {
+	mb := &MemoryBackend{Entries: MockEntries()}
+	se, _ := mb.GetSnapshotBefore(TestBaseTime.Add(time.Duration(-9) * time.Second))
+	if se == nil {
+		t.Fail()
+	}
+	if se.Data["test"].(int) != 1 {
 		t.Fail()
 	}
 }
